c9b20873d93bf71d2e68a38270dd50032c84649e
Steven4Jesus <Steven4Jesus@0c3aa028-4a46-be0a-df05-f9b5da6e5ad4>
2010-03-31 15:05:35 +0000

    Vollstaendige Implementierung von der Klasse Spielzug

--- LoGo/src/Klassen/Spielfeld.java
+++ LoGo/src/Klassen/Spielfeld.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Klassen;
 
 import java.util.List;
@@ -15,36 +14,34 @@ import java.util.List;
 public class Spielfeld {
 
     // interne Speicherobjekte
-    private int             letzteZugnummer;
-    private int             spielfeldGroesse;
-    private int             gefangenenAnzahlWeiss;
-    private int             gefangenenAnzahlSchwarz;
-    private List <Spielzug> spielZugCollection;
-
+    private int letzteZugnummer;
+    private int spielfeldGroesse;
+    private int gefangenenAnzahlWeiss;
+    private int gefangenenAnzahlSchwarz;
+    private List<Spielzug> spielZugCollection;
+    
     // Chache Funktionen zum schnelleren Spielen
-    private int[][]         aktuellesSpielfeldCache;
-    private int             spielfeldCacheMitZugnummerStand;
+    private int[][] aktuellesSpielfeldCache;
+    private int spielfeldCacheMitZugnummerStand;
 
-
-    public void Spielfeld( int spielfeldGroesse ){
-        this.spielfeldGroesse   = spielfeldGroesse;
-        this.letzteZugnummer    = 0;
+    public void Spielfeld(int spielfeldGroesse) {
+        this.spielfeldGroesse = spielfeldGroesse;
+        this.letzteZugnummer = 0;
         this.setGefangenenAnzahlSchwarz(0);
         this.setGefangenenAnzahlWeiss(0);
 
 
         // Chache Funktionen setzen
-        aktuellesSpielfeldCache         = new int[this.getSpielfeldGroesse()][this.getSpielfeldGroesse()];
+        aktuellesSpielfeldCache = new int[this.getSpielfeldGroesse()][this.getSpielfeldGroesse()];
         spielfeldCacheMitZugnummerStand = this.letzteZugnummer;
 
     }
-
 
     /**
      * 
      * @return Die Nummer des zuletzt gespielten Zuges
      */
-    public int getLetzteZugnummer(){
+    public int getLetzteZugnummer() {
         return this.letzteZugnummer;
     }
 
@@ -52,7 +49,7 @@ public class Spielfeld {
      *
      * @return Größe des aktuellen Spielfeldes
      */
-    public int getSpielfeldGroesse(){
+    public int getSpielfeldGroesse() {
         return this.spielfeldGroesse;
     }
 
@@ -60,7 +57,7 @@ public class Spielfeld {
      *
      * @return Anzahl der gefangenen schwarzen Stein (Punkte für weiss)
      */
-    public int getGefangenenAnzahlWeiss(){
+    public int getGefangenenAnzahlWeiss() {
         return this.gefangenenAnzahlWeiss;
     }
 
@@ -68,7 +65,7 @@ public class Spielfeld {
      *
      * @param Speichern der Anzahl der gefangenen schwarzen Stein (Punkte für weiss)
      */
-    public void setGefangenenAnzahlWeiss( int gefangenenAnzahl ){
+    public void setGefangenenAnzahlWeiss(int gefangenenAnzahl) {
         this.gefangenenAnzahlWeiss = gefangenenAnzahl;
     }
 
@@ -76,33 +73,32 @@ public class Spielfeld {
      *
      * @return Anzahl der gefangenen weissen Stein (Punkte für schwarz)
      */
-    public int getGefangenenanzahlSchwarz(){
+    public int getGefangenenanzahlSchwarz() {
         return this.gefangenenAnzahlSchwarz;
     }
-    
+
     /**
      * 
      * @param Anzahl der gefangenen weissen Stein (Punkte für schwarz)
      */
-    public void setGefangenenAnzahlSchwarz( int gefangenenAnzahl ){
+    public void setGefangenenAnzahlSchwarz(int gefangenenAnzahl) {
         this.gefangenenAnzahlSchwarz = gefangenenAnzahl;
     }
-
 
     /**
      *
      * @return Gibt das aktuelle Spielfeld als Array zurück
      */
-    public int[][] getAktuelesSpielFeld(){
+    public int[][] getAktuelesSpielFeld() {
 
         int spielfeld[][];
 
-        if(this.spielfeldCacheMitZugnummerStand != this.letzteZugnummer){
-             spielfeld = new int[this.getSpielfeldGroesse()][this.getSpielfeldGroesse()];
+        if (this.spielfeldCacheMitZugnummerStand != this.letzteZugnummer) {
+            spielfeld = new int[this.getSpielfeldGroesse()][this.getSpielfeldGroesse()];
 
             // Spielfeld initialisieren
-            for(int i=this.getSpielfeldGroesse()-1; i>=0; i--){
-                for(int j=this.getSpielfeldGroesse()-1; j>=0; j--){
+            for (int i = this.getSpielfeldGroesse() - 1; i >= 0; i--) {
+                for (int j = this.getSpielfeldGroesse() - 1; j >= 0; j--) {
                     spielfeld[j][i] = Konstante.SCHNITTPUNKT_LEER;
                 }
             }
@@ -116,8 +112,7 @@ public class Spielfeld {
              *
              */
 
-        }
-        else{
+        } else {
             spielfeld = this.aktuellesSpielfeldCache;
         }
 
@@ -131,24 +126,23 @@ public class Spielfeld {
      * da das Spielfeld hierbei immer neu generiert werden muss
      * @return Spielfeld zum übergebenen Zeipunkt in Form eines Arrays
      */
-    public int[][] getSpielfeldZumZeitpunkt( int zeitpunkt ){
+    public int[][] getSpielfeldZumZeitpunkt(int zeitpunkt) {
 
         int spielfeld[][];
 
-        if( zeitpunkt == this.spielfeldCacheMitZugnummerStand ){
+        if (zeitpunkt == this.spielfeldCacheMitZugnummerStand) {
             spielfeld = this.aktuellesSpielfeldCache;
-        }
-        else{
-             spielfeld = new int[this.getSpielfeldGroesse()][this.getSpielfeldGroesse()];
+        } else {
+            spielfeld = new int[this.getSpielfeldGroesse()][this.getSpielfeldGroesse()];
 
             // Spielfeld initialisieren
-            for(int i=this.getSpielfeldGroesse()-1; i>=0; i--){
-                for(int j=this.getSpielfeldGroesse()-1; j>=0; j--){
+            for (int i = this.getSpielfeldGroesse() - 1; i >= 0; i--) {
+                for (int j = this.getSpielfeldGroesse() - 1; j >= 0; j--) {
                     spielfeld[j][i] = Konstante.SCHNITTPUNKT_LEER;
                 }
             }
 
-             /*
+            /*
              * Hier muss jetzt noch nach dem initialisieren die Werte aus der Collection
              * abgefragt werden und ins Feld eingetragen werden
              */
@@ -164,7 +158,7 @@ public class Spielfeld {
      * weitergespielt.
      * @return True, wenn die Funktion erfolgreich ausgeführt wurde, sonst false
      */
-    public boolean setSpielfeldZumZeitpunkt( int zeitpunkt ){
+    public boolean setSpielfeldZumZeitpunkt(int zeitpunkt) {
         /*
          * Setzen des Spiels auf den Zeitpunkt:
          * - prüfen ob Möglich
@@ -186,7 +180,7 @@ public class Spielfeld {
      * Diese kann Werte zwischen 1 und der Feldlänge enthalten
      *
      */
-    public void setStein( int xPos, int yPos ){
+    public void setStein(int xPos, int yPos) {
         /* Die Funktion setzt den Stein fuer abhaengig vom letzten gespielten Zug.
          * Wenn also Spieler schwarz den letzten Zug gesetzt hat, wird der neue
          * Zug für Spieler weiss eingetragen.
@@ -206,13 +200,13 @@ public class Spielfeld {
      * Diese kann Werte zwischen 1 und der Feldlänge enthalten
      * @param Farbe des setzenden Spielers
      */
-    public void setStein( int xPos, int yPos, int spielerfarbe ){
+    public void setStein(int xPos, int yPos, int spielerfarbe) {
         /*
          * siehe Funktion setStein(...)
          */
     }
 
-    public boolean setSteinMoeglich( int xPos, int yPos ){
+    public boolean setSteinMoeglich(int xPos, int yPos) {
         /*
          * Prüft ab ob an diesem Punkt ein Stein gesetzt werden darf.
          * Funktion wird verwendet, wenn zum Beispiel über einen Schnittpunkt
@@ -226,6 +220,4 @@ public class Spielfeld {
 
         return true;
     }
-
-
 }
