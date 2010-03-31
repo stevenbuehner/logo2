6f8755d413ec2561a7fe8f15c51e7cf8ecb4a020
Steven4Jesus <Steven4Jesus@0c3aa028-4a46-be0a-df05-f9b5da6e5ad4>
2010-03-31 15:05:35 +0000

    Vollstaendige Implementierung von der Klasse Spielzug

--- trunk/LoGo/src/Klassen/Spielzug.java
+++ trunk/LoGo/src/Klassen/Spielzug.java
@@ -2,10 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Klassen;
 
- /**
+/**
  *
  * @author steven
  * @version 0.1
@@ -13,16 +12,79 @@ package Klassen;
  */
 public class Spielzug {
 
-    private int xPos;
-    private int yPos;
+    private int xPosition;
+    private int yPosition;
     private int farbe;
 
-    public void setXPos( int xPos ){
-        this.xPos = xPos;
+    /**
+     * Konstruktur der Klasse
+     * @param X-Position des Spielzuges
+     * @param Y-Position des Spielzuges
+     * @param Spielerfarbe des Steins der gelegt wurde. zur Auswahl stehen
+     * NUR die Konstanten Konstante.SCHNITTPUNKT_SCHWARZ und
+     * Konstante.SCHNITTPUNKT_WEISS. Wurde ein anderer Wert uebergeben wird
+     * automatisch der Wert Konstante.SCHNITTPUNKT_LEER gesetzt.
+     */
+    public void Spielzug(int xPos, int yPos, int farbe) {
+        this.setXPosition(xPos);
+        this.setYPosition(yPos);
+        this.setFarbe(farbe);
     }
 
+    /**
+     *
+     * @return X-Position des Spielerzuges
+     */
+    public int getXPosition() {
+        return xPosition;
+    }
+
+    /**
+     *
+     * @param X-Position des Spielerzuges
+     */
+    public void setXPosition(int val) {
+        this.xPosition = val;
+    }
+
+    /**
+     *
+     * @return Y-Position des Spielzuges
+     */
+    public int getYPosition() {
+        return yPosition;
+    }
+
+    /**
+     *
+     * @param Y-Position des Spielzuges
+     */
+    public void setYPosition(int val) {
+        this.yPosition = val;
+    }
+
+    /**
+     * @return Spielerfarbe des Steins der gelegt wurde. zur Auswahl stehen
+     * NUR die Konstanten Konstante.SCHNITTPUNKT_SCHWARZ und
+     * Konstante.SCHNITTPUNKT_WEISS. Wurde ein anderer Wert uebergeben wird
+     * automatisch der Wert Konstante.SCHNITTPUNKT_LEER gesetzt.
+    */
+    public int getFarbe() {
+        return this.farbe;
+    }
     
-
-
-
+    /**
+     * @param Spielerfarbe des Steins der gelegt wurde. zur Auswahl stehen
+     * NUR die Konstanten Konstante.SCHNITTPUNKT_SCHWARZ und
+     * Konstante.SCHNITTPUNKT_WEISS. Wurde ein anderer Wert uebergeben wird
+     * automatisch der Wert Konstante.SCHNITTPUNKT_LEER gesetzt.
+    */
+    public void setFarbe(int farbe) {
+        if (farbe == Konstante.SCHNITTPUNKT_SCHWARZ ||
+                farbe == Konstante.SCHNITTPUNKT_WEISS) {
+            this.farbe = farbe;
+        } else {
+            this.farbe = Konstante.SCHNITTPUNKT_LEER;
+        }
+    }
 }
