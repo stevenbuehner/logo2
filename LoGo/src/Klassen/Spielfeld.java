8b9136abd63d8ab4967cb96e7415c57ddcc483fd
steven4jesus <steven4jesus@0c3aa028-4a46-be0a-df05-f9b5da6e5ad4>
2010-04-03 11:06:27 +0000

    AnalyseSchnittpunkt-Klasse ausgebaut und funktion angefangen, die steine vom brett nehmen soll

--- LoGo/src/Klassen/Spielfeld.java
+++ LoGo/src/Klassen/Spielfeld.java
@@ -36,7 +36,13 @@ public class Spielfeld {
         // Chache Funktionen setzen
         aktuellesSpielfeldCache = new int[this.getSpielfeldGroesse()][this.getSpielfeldGroesse()];
         spielfeldCacheMitZugnummerStand = this.letzteZugnummer;
-
+        /* Feld muss als Leeres Feld initialisiert werden */
+        int i,j;
+        for(i=0; i<spielfeldGroesse; i++){
+            for(j=0; i<spielfeldGroesse; j++){
+                aktuellesSpielfeldCache[i][j] = Konstante.SCHNITTPUNKT_LEER;
+            }
+        }
     }
 
     /**
@@ -64,8 +70,7 @@ public class Spielfeld {
     }
 
     /**
-     *
-     * @param Speichern der Anzahl der gefangenen schwarzen Stein (Punkte für weiss)
+     * @param gefangenenAnzahl Speichern der Anzahl der gefangenen schwarzen Stein (Punkte für weiss)
      */
     public void setGefangenenAnzahlWeiss(int gefangenenAnzahl) {
         this.gefangenenAnzahlWeiss = gefangenenAnzahl;
@@ -80,8 +85,7 @@ public class Spielfeld {
     }
 
     /**
-     * 
-     * @param Anzahl der gefangenen weissen Stein (Punkte für schwarz)
+     * @param gefangenenAnzahl Anzahl der gefangenen weissen Stein (Punkte für schwarz)
      */
     public void setGefangenenAnzahlSchwarz(int gefangenenAnzahl) {
         this.gefangenenAnzahlSchwarz = gefangenenAnzahl;
@@ -125,7 +129,7 @@ public class Spielfeld {
 
     /**
      *
-     * @param Zeipunkt zu dem das Spielfeld zurückgegeben werden soll.
+     * @param zeitpunkt Zeipunkt zu dem das Spielfeld zurückgegeben werden soll.
      * In erster Linie für REDO und UNDO gedacht, nicht zum häufigen aufrufen,
      * da das Spielfeld hierbei immer neu generiert werden muss
      * @return Spielfeld zum übergebenen Zeipunkt in Form eines Arrays
@@ -157,7 +161,7 @@ public class Spielfeld {
 
     /**
      *
-     * @param Zeitpunkt auf den das Spielfeld FEST gesetzt werden soll.
+     * @param zeitpunkt Zeitpunkt auf den das Spielfeld FEST gesetzt werden soll.
      * Ein Redo ist dann nicht mehr möglich. Das Spiel wird zu diesem Zeitpunkt
      * weitergespielt.
      * @return True, wenn die Funktion erfolgreich ausgeführt wurde, sonst false
@@ -178,11 +182,11 @@ public class Spielfeld {
 
     /**
      * 
-     * @param X-Position des Spielfelds. 
+     * @param xPos X-Position des Spielfelds.
      * Diese kann Werte zwischen 1 und der Feldlänge enthalten
-     * @param Y-Position des Spielfelds. 
+     * @param yPos Y-Position des Spielfelds.
      * Diese kann Werte zwischen 1 und der Feldlänge enthalten
-     *
+     * @return Gibt zurueck, ob das setzen erfolgreich war
      */
     public boolean setStein(int xPos, int yPos) {
 
@@ -221,6 +225,10 @@ public class Spielfeld {
         return false; // Erstmal false machen... sollte geaendert werden wenn die Funktion was macht!!
     }
 
+    /*
+     * Wollen wir die Funktion loeschen? Ich waere dafuer, da wir sie wahrscheinlich nicht
+     * brauchen. gez tommy
+     */
     public boolean setSteinMoeglich( int xPos, int yPos, int spielerfarbe ) {
         /*
          * Prüft ab ob an diesem Punkt ein Stein gesetzt werden darf.
@@ -260,6 +268,82 @@ public class Spielfeld {
              gelöscht wurden.
          */
         return this.spielZugCollection.get(this.spielZugCollection.size()-1).getFarbe();
+    }
+
+
+    /**
+     * Diese Funktion soll Steine vom Spielfeld nehmen, wenn dies moeglich ist.
+     * Dabei benoetigt sie eine Koordinate mit der sie Beginnen kann, damit klar
+     * ist, welche Gruppe gemeint ist. Von dieser Koordinate aus versucht sie dann
+     * Steine vom Brett zu entfernen.
+     * @param xPos Ist die X-Koordinate des Steines, der zur Gruppe gehoert,
+     * die vom Brett zu nehmen versucht wird.
+     * @param yPos Ist die Y-Koordinate des Steins.
+     * @param Feld Ist ein 2-Dimensionales Array vom Typ AnalyseSchnittpunkt. Es
+     * dient als Brettstellung, die zu betrachten ist.
+     * @return Der Rueckgabewert kennzeichnet bei Erfolg (also wenn die Gruppe
+     * vom Brett genommen werden konnte) wieviele Steine entfernt worden. Bei
+     * Misserfolg gibt es 2 Moeglichkeiten: 1. Die Gruppe konnte nicht entfernt
+     * werden, da sie noch Freiheiten besitzt (Rueckgabewert 0). 2. Die Gruppe
+     * konnte nicht entfernt werden, weil an (xPos, yPos) gar kein Stein liegt
+     * (Rueckgabewert -1)
+     */
+    private int versucheSteinZuNehmen(int xPos, int yPos, AnalyseSchnittpunkt feld[][]){
+        /* Zuerst testen ob an (x,y) ueberhaupt ein Stein ist */
+        if(feld[xPos][yPos].getBelegungswert()==Konstante.SCHNITTPUNKT_LEER || feld[xPos][yPos].getBelegungswert()==Konstante.SCHNITTPUNKT_VERBOTEN){
+            return -1;
+        }
+
+        /* Noch abfangen, ob Wert falsch ist, denn logischer weise sollte jetzt
+         * der Belegungswert schwarz oder weiss sein */
+        if(feld[xPos][yPos].getBelegungswert()!=Konstante.SCHNITTPUNKT_SCHWARZ && feld[xPos][yPos].getBelegungswert()!=Konstante.SCHNITTPUNKT_WEISS){
+            return -2;
+        }
+
+        /* Jetzt beginnt die eigentliche Prozedur, da man weiss, das es sich entweder
+         * um einen schwarzen oder einen weissen Stein handelt. Zuerst wird getestet ob die
+         * Steine schon betrachtet wurden. Ist dies der Fall wird abgebrochen und
+         * gesagt, das keine Steine gefangen worden (Da diese wenn sie schon
+         * markiert sind auch schon vom Brett genommen worden, oder nicht)*/
+        if(feld[xPos][yPos].getSteinStatus()==Konstante.STEIN_GEFANGEN || feld[xPos][yPos].getSteinStatus()==Konstante.STEIN_LEBENDIG){
+            return 0; // nichts veraendert
+        }
+
+        /* Es muss also was veraendert werden. Am ende der Funktion werden
+         * entweder alle Steine der Gruppe als lebendig markiert, oder alle als
+         * tot markiert und vom Brett (richtigen spielfeld) genommen. */
+        boolean fangbar = true;
+        int feldGroesse = this.getSpielfeldGroesse();
+        int farbe = feld[xPos][yPos].getBelegungswert();
+        AnalyseSchnittpunkt [] listeSteine;             // Das sollte man mit ner
+        listeSteine = new AnalyseSchnittpunkt[362];     // Collection auch loesen koennen
+        int momElement = 0;
+        int endElement = 1;
+        feld[xPos][yPos].setMarkiert(true);
+        listeSteine[0] = feld[xPos][yPos];
+        /* Jetzt solange nach Steinen suchen und diese in Liste aufnehmen,
+         * bis keine Steine mehr da sind */
+        do{
+            /* Jetzt, wenn Nachbarsteine existieren und diese noch nicht
+             * markiert sind, dann werden diese in Liste aufgenommen.*/
+
+            /* Wenn Stein linken Nachbarn hat*/
+             if(listeSteine[momElement].getXPos()!=0){
+                 /* Nachsehen, ob linker nachbar Leer, dann Gruppe nicht fangbar! */
+                 if(feld[listeSteine[momElement].getXPos()-1][listeSteine[momElement].getYPos()].getBelegungswert() == Konstante.SCHNITTPUNKT_LEER ||
+                    feld[listeSteine[momElement].getXPos()-1][listeSteine[momElement].getYPos()].getBelegungswert() == Konstante.SCHNITTPUNKT_VERBOTEN){
+                     fangbar=false; // Gruppe nicht fangbar, da sie eine Freiheit hat.
+                 }
+                 /* Der Stein hat also einen Nachbarstein, wenn dieser nicht der
+                  * Steinfarbe entspricht, muss er auch nicht in die Liste
+                  * Aufgenommen werden. Hat er allerdings die gleiche Farbe,
+                  * wird er markiert und in die Liste aufgenommen */
+                 else if(feld[listeSteine[momElement].getXPos()-1][listeSteine[momElement].getYPos()].getBelegungswert() == farbe){
+                     
+                 }
+             }
+        }while(momElement<endElement);
+        return 0;  // Damit NetBeans erstmal nicht meckert
     }
 
 }
