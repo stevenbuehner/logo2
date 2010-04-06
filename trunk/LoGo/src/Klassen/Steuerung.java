7cc1f2849fe73b79eeaf54836e94850c782f82a4
Steven4Jesus <Steven4Jesus@0c3aa028-4a46-be0a-df05-f9b5da6e5ad4>
2010-04-06 11:13:49 +0000

    Interface der Steuerung implementiert (ohne Inhalt)

--- trunk/LoGo/src/Klassen/Steuerung.java
+++ trunk/LoGo/src/Klassen/Steuerung.java
@@ -5,13 +5,15 @@
 
 package Klassen;
 
+import interfaces.SteuerungIntface;
+
 /**
  *
  * @author steven
  * @version 0.1
  * @param Die Klasse dient zum Verwalten der Daten eines Spielers
  */
-public class Steuerung {
+public class Steuerung implements SteuerungIntface {
 
     private Spieler spielerSchwarz;
     private Spieler spielerWeiss;
@@ -25,6 +27,82 @@ public class Steuerung {
 
     public void Steuerung( ){
 
+    }
+
+    public void initMitEinstellungen(String spielerName1, String spielerName2, long spielZeit1, long spielZeit2, float komiFuerWeiss, int spielfeldGroesse, int vorgabeSteineFuerSchwarz) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void initMitEinstellungen(String spielerName1, String spielerName2, long spielZeit1, long spielZeit2, float komiFuerWeiss, int spielfeldGroesse) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void initMitEinstellungen(String spielerName1, String spielerName2, long spielZeit1, long spielZeit2, int spielfeldGroesse) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void initMitEinstellungenFuerStartformation(String spielerName1, String spielerName2, long spielZeit1, long spielZeit2, float komiFuerWeiss, int spielfeldGroesse) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void initMitEinstellungenFuerStartformation(String spielerName1, String spielerName2, long spielZeit1, long spielZeit2, int spielfeldGroesse) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void initMitDatenModell(Spielfeld feld, String spielerName1, String spielerName2, long spielZeit1, long spielZeit2, float komiFuerWeiss) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void klickAufFeld(int xPos, int yPos) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void buttonAufgeben() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void buttonPassen() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void buttonPause() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void buttonSpielSpeichern() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void buttonUndo() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void buttonRedo() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void buttonToStart() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void buttonToEnd() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void zeitAbgelaufenSpieler1Hauptzeit() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void zeitAbgelaufenSpieler1Periodenzeit() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void zeitAbgelaufenSpieler2Hauptzeit() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void zeitAbgelaufenSpieler2Periodenzeit() {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
 
