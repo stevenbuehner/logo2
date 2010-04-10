7faa967a61a986bf48e2571e21c4e047e47f940e
t.schladitz <t.schladitz@0c3aa028-4a46-be0a-df05-f9b5da6e5ad4>
2010-04-10 20:37:52 +0000

    Klasse SpielAuswertung: Einige Methodenruempfe definiert

--- LoGo/src/Klassen/SpielAuswertung.java
+++ LoGo/src/Klassen/SpielAuswertung.java
@@ -16,6 +16,7 @@ public class SpielAuswertung {
     private int gefangeneVonWeiss;
     private AnalyseSchnittpunkt auswertungBrett[][];
     private int brettGroesse;
+    private int eingegebenesFeld[][];
 
     /**
      * Der Konstruktor wird erst aufgerufen wenn das Spiel ausgewertet werden
@@ -32,6 +33,12 @@ public class SpielAuswertung {
         for(int i=0; i<this.getFeldGroesse(); i++){
             for(int j=0; j<this.getFeldGroesse(); j++){
                 this.auswertungBrett[i][j] = new AnalyseSchnittpunkt(i,j,Konstante.SCHNITTPUNKT_LEER);
+            }
+        }
+        this.eingegebenesFeld = new int[this.getFeldGroesse()][this.getFeldGroesse()];
+        for(int i=0; i<this.getFeldGroesse(); i++){
+            for(int j=0; j<this.getFeldGroesse(); j++){
+                this.eingegebenesFeld[i][j] = Konstante.SCHNITTPUNKT_LEER;
             }
         }
     }
@@ -80,5 +87,83 @@ public class SpielAuswertung {
      */
     private int getFeldGroesse(){
         return this.brettGroesse;
+    }
+
+    /**
+     * Diese Funktion liest das auszuwertende Feld ein. Bevor irgendetwas berechnet
+     * wird, sollte das Feld initialisiert werden. Sonst ist es Leer. Dabei wird
+     * kein Unterschied zwischen einem Verbotenen Punkt und einem Leeren Schnittpunkt
+     * gemacht. Der Grund ist, weil sowieso nicht weiter gespielt wird und der
+     * Schnittpunkt daher als leer anzusehen ist.
+     * @param feld Zu initialisierendes Feld (2-Dim-Array)
+     */
+    public void auswertungInitialisieren(int feld[][]){
+        for(int i=0; i<this.getFeldGroesse(); i++){
+            for(int j=0; j<this.getFeldGroesse(); j++){
+                switch(feld[i][j]){
+                    case Konstante.SCHNITTPUNKT_SCHWARZ :
+                        this.eingegebenesFeld[i][j] = Konstante.SCHNITTPUNKT_SCHWARZ;
+                        this.auswertungBrett[i][j].setBelegungswert(Konstante.SCHNITTPUNKT_SCHWARZ);
+                        break;
+                    case Konstante.SCHNITTPUNKT_WEISS :
+                        this.eingegebenesFeld[i][j] = Konstante.SCHNITTPUNKT_WEISS;
+                        this.auswertungBrett[i][j].setBelegungswert(Konstante.SCHNITTPUNKT_WEISS);
+                        break;
+                    case Konstante.SCHNITTPUNKT_LEER :
+                    case Konstante.SCHNITTPUNKT_VERBOTEN:
+                        this.eingegebenesFeld[i][j] = Konstante.SCHNITTPUNKT_LEER;
+                        this.auswertungBrett[i][j].setBelegungswert(Konstante.SCHNITTPUNKT_LEER);
+                        break;
+                    default : /* Hierer darf man nicht kommen */
+                        throw new UnsupportedOperationException("Das eingegebene Feld enthaelt unerwartete Werte an Stelle (" + i + "|" + j + ")");
+                }
+            }
+        }
+    }
+
+    /**
+     * Damit man das Feld auswerten kann, muss der Benutzer signalisieren, welche
+     * Steine auf dem Brett tot sind.
+     * @param xPos X-Koordinate
+     * @param yPos Y-Koordinate
+     */
+    public void markiereSteinAlsGefangen(int xPos, int yPos){
+
+    }
+
+    /**
+     * Moechte der Benutzer eine Markierung rueckgaengig machen, kann er das
+     * mit dieser Funktion. Es ist Aufgabe der Steuerung, ob der Benutzer
+     * einen Stein tot oder untot markieren will.
+     * @param xPos X-Koordinate
+     * @param yPos Y-Koordinate
+     */
+    public void markiereSteinAlsNichtGefangen(int xPos, int yPos){
+
+    }
+
+    /**
+     * Wenn bei der Auswertung irgendwas Schief laeft (Weil der Benutzer aus
+     * Versehen viele Gruppen als Tot markiert hat, die in Wirklichkeit leben)
+     * kann die Auswertung auch einfach neu gestartet werden.
+     */
+    public void initialisiereNeu(){
+
+    }
+
+    /**
+     * Um das Ergebnis der Auswertung sichtbar zu machen, wird ein Feld zurueckgegeben,
+     * bei dem man sieht, welche Teile des Brettes schon wie ausgewertet wurden
+     * @return Brett mit folgenden moeglichen Werten:
+     * - Konstante.SCHNITTPUNKT_SCHWARZ
+     * - Konstante.SCHNITTPUNKT_WEISS
+     * - Konstante.SCHNITTPUNKT_LEER
+     * - Konstante.SCHNITTPUNKT_GEBIET_SCHWARZ
+     * - Konstante.SCHNITTPUNKT_GEBIET_WEISS
+     */
+    public int[][] getAusgewertetesFeld(){
+        // damit NetBeans die klappe haelt
+        int a[][] = new int[1][1];
+        return a;
     }
 }
