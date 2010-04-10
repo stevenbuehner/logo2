1bd9bc7480e6b4003e0c94b85f2aaa11cba897c1
t.schladitz <t.schladitz@0c3aa028-4a46-be0a-df05-f9b5da6e5ad4>
2010-04-10 19:49:49 +0000

    Neue Klasse SpielAuswertung erstellt. Soll Spaeter das Ergebnis des Spiels mit hilfe des Benutzers ermitteln

---
+++ LoGo/src/Klassen/SpielAuswertung.java
@@ -0,0 +1,84 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Klassen;
+
+/**
+ * Diese Klasse uebernimmt die Auswertung eines Spielfeldes
+ * Dabei werden Gebietspunkte, Gefangenenpunkte und Komi beruecksichtigt
+ * @author tommy
+ */
+public class SpielAuswertung {
+    private int komiFuerWeiss;
+    private int gefangeneVonSchwarz;
+    private int gefangeneVonWeiss;
+    private AnalyseSchnittpunkt auswertungBrett[][];
+    private int brettGroesse;
+
+    /**
+     * Der Konstruktor wird erst aufgerufen wenn das Spiel ausgewertet werden
+     * soll. Brettgroesse und Komi sind daher bekannt
+     * @param brettGroesse Bettgroesse
+     * @param komiFuerWeiss Komi: Punkte werden Weiss gutgeschrieben.
+     */
+    public SpielAuswertung(int brettGroesse, int komiFuerWeiss){
+        this.brettGroesse = brettGroesse;
+        this.komiFuerWeiss = komiFuerWeiss;
+        this.setGefangeneVonSchwarz(0);
+        this.setGefangeneVonWeiss(0);
+        this.auswertungBrett = new AnalyseSchnittpunkt[this.getFeldGroesse()][this.getFeldGroesse()];
+        for(int i=0; i<this.getFeldGroesse(); i++){
+            for(int j=0; j<this.getFeldGroesse(); j++){
+                this.auswertungBrett[i][j] = new AnalyseSchnittpunkt(i,j,Konstante.SCHNITTPUNKT_LEER);
+            }
+        }
+    }
+
+    public SpielAuswertung(int brettGroesse){
+        this(brettGroesse, 0);
+    }
+
+    /**
+     * Legt die Zahl der Steine fest, die der schwarze Spieler gefangen hat.
+     * @param gefangeneVonSchwarz Anzahl der Steine
+     */
+    public void setGefangeneVonSchwarz(int gefangeneVonSchwarz){
+        this.gefangeneVonSchwarz = gefangeneVonSchwarz;
+    }
+
+    /**
+     * Gibt die Anzahl der Steine wieder, die als Gefangenenanzahl fuer Schwarz
+     * eingestellt wurden.
+     * @return Anzahl der Steine
+     */
+    public int getGefangeneVonSchwarz(){
+        return this.gefangeneVonSchwarz;
+    }
+
+    /**
+     * Legt die Zahl der Steine fest, die der weisse Spieler gefangen hat.
+     * @param gefangeneVonWeiss Anzahl der Steine
+     */
+    public void setGefangeneVonWeiss(int gefangeneVonWeiss){
+        this.gefangeneVonWeiss = gefangeneVonWeiss;
+    }
+
+    /**
+     * Gibt die Anzahl der Steine wieder, die als Gefangenenanzahl fuer Schwarz
+     * eingestellt wurden.
+     * @return Anzahl der Steine
+     */
+    public int getGefangeneVonWeiss(){
+        return this.gefangeneVonWeiss;
+    }
+
+    /**
+     * Fuer interne Zwecke. Gibt die eingestellte Feldgroesse wieder.
+     * @return Feldgroesse  (auswertungBrett)
+     */
+    private int getFeldGroesse(){
+        return this.brettGroesse;
+    }
+}
