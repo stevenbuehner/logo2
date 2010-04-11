15f3dfbe3274845cca3c5487396402970382c9de
t.schladitz <t.schladitz@0c3aa028-4a46-be0a-df05-f9b5da6e5ad4>
2010-04-11 16:08:25 +0000

    Neue Konstanten fuer Auswertungsfunktion definiert. Diese Konstanten muss die GUI bei der Auswertung auch anzeigen.

--- trunk/LoGo/src/Klassen/Konstante.java
+++ trunk/LoGo/src/Klassen/Konstante.java
@@ -24,9 +24,10 @@ public class Konstante {
     public final static int STEIN_LEBENDIG              = 6;
 
     // Konstanten fuer Spielauswertung
-    public final static int SCHNITTPUNKT_NEUTRAL        = 7;
     public final static int SCHNITTPUNKT_GEBIET_SCHWARZ = 8;
     public final static int SCHNITTPUNKT_GEBIET_WEISS   = 9;
+    public final static int SCHNITTPUNKT_SCHWARZ_GEFANGEN = 10;
+    public final static int SCHNITTPUNKT_WEISS_GEFANGEN = 11;
 
     // Konstanten fuer den Zustand des Spielfeldes
     public final static int SPIEL_UNVOLLSTAENDIG        = 100;
