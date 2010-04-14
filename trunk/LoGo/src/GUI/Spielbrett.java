e68f2071fa6b4a019d7ea590d356852209d3797c
Steven4Jesus <Steven4Jesus@0c3aa028-4a46-be0a-df05-f9b5da6e5ad4>
2010-04-14 15:05:38 +0000

    Spielbrett bekommt Felder, implementiert die Interfaces Drawable und Movable

--- trunk/LoGo/src/GUI/Spielbrett.java
+++ trunk/LoGo/src/GUI/Spielbrett.java
@@ -1,11 +1,11 @@
-
 package GUI;
 
+import interfaces.Drawable;
+import interfaces.Movable;
 import java.awt.Canvas;
-import java.awt.Color;
 import java.awt.Graphics;
-
-
+import java.awt.Image;
+import java.awt.image.BufferedImage;
 
 /**
  *
@@ -16,54 +16,150 @@ import java.awt.Graphics;
  *
  * Diese Klasse wird aktuell nirgends im Spiel verwedet!!!!
  */
-public class Spielbrett extends Canvas
-{
+public class Spielbrett extends Canvas implements Drawable, Movable{
+
+    private int brettBreite;
+    private int brettHoehe;
+    private int xOffset;
+    private int yOffset;
+    private int anzahlFelder;
+    private int[][] spielFeldArray;
+
+    // Grafiken
+    private Image backgoundImage;
+    private SpielStein[][] feld;
+        
     //int state = 0;
-    private int     i;
-    private int     wert    = 20;
-    private int     x       = 20;
-    private int     y       = 20;
+    private int wert = 20;
+
     private int[][] array;
 
     // Array-Inhalte alle auf 0 setzen
-    public Spielbrett()
-    {
-        array = new int[x][y];
-        for(int m = 0; m < x; m++)
-        {
-            for(int n = 0; n < x; n++)
-            {
-                array[m][n] = 0;
+    public Spielbrett(int breite, int hoehe,int xOffset,int yOffset, int anzahlFelder, Image backgroundImage) {
+        this.brettBreite = breite;
+        this.brettHoehe = hoehe;
+        this.xOffset = xOffset;
+        this.yOffset = yOffset;
+        this.anzahlFelder = anzahlFelder;
+        this.backgoundImage = backgroundImage;
+
+        // initialisiere das Spielfeld mit leeren Feldern
+        this.spielFeldArray = new int[anzahlFelder][anzahlFelder];
+        for (int m = 0; m < xOffset; m++) {
+            for (int n = 0; n < xOffset; n++) {
+                spielFeldArray[m][n] = 0;
             }
         }
+
+        //Berechnung zur Initialisierung
+        double feldHoehe = brettHoehe/anzahlFelder;
+        double feldBreite = brettBreite/anzahlFelder;
+
+        // Spielbrett-Grafiken laden
+        GrafikLib lib = GrafikLib.getInstance();
+        BufferedImage[] bi = lib.getSprite("Spielsteine_6x5.png", 6, 5);
+        for (int m = 0; m < xOffset; m++) {
+            for (int n = 0; n < xOffset; n++) {
+                feld[m][n] = new SpielStein(bi, xOffset+feldBreite*m, yOffset +feldHoehe*n, 20);
+            }
+        }
+
+
+
     }
 
     @Override
-    public void paint(Graphics g)
-    {
-        // Gitter zeichnen
-        for(i=0;i<=x;i++)
-        {
-            g.drawLine(wert*i, wert*x, wert*i , 0);
+    public void paint(Graphics g) {
+
+        if(backgoundImage != null)
+            g.drawImage(backgoundImage, xOffset, yOffset, this);
+        
+        //Berechnung fuer das Feld
+        int feldHoehe = brettHoehe/anzahlFelder;
+        int feldBreite = brettBreite/anzahlFelder;
+        int halbeFeldHoehe = feldHoehe/2;
+        int halbeFeldBreite = feldBreite/2;
+
+
+        
+        // Horizontale Linien zeichnen
+        for (int i = 0; i < this.anzahlFelder; i++) {
+            g.drawLine(this.xOffset, this.yOffset+i*feldHoehe, xOffset+brettBreite, yOffset);
         }
 
-        for(i=0;i<=y;i++)
-        {
-            g.drawLine(0 , wert*i, wert*y, wert*i);
+        // Vertikale Linien zeichnen
+        for (int i = 0; i < this.anzahlFelder; i++) {
+            g.drawLine(xOffset + i*feldBreite, yOffset, xOffset + i*feldBreite, yOffset+brettHoehe);
         }
 
-        // Zellen ausfüllen, bei denen das Array den Inhalt "1" hat
-        for(int k = 0; k < x; k++)
-        {
-            for(int l = 0; l < y; l++)
-            {
-                if(array[k][l] == 1)
-                {
-                    g.setColor(Color.BLACK);
-                    g.fillRect(0+k*wert,0+l*wert,wert,wert);
+        // Spielsteine zeichnen
+        for(int i=0; i< this.anzahlFelder; i++){
+
+        }
+
+    }
+
+    /**
+     * Versuch nur die benötigten / veränderten Felder neu zu zeiche ... mal
+     * schaun ob sich das umsetzen lässt
+     * @param g
+     * @param neuesFeld
+     */
+    private void zeichneBenoetigteFelderNeu(Graphics g, int neuesFeld[][]) {
+
+        // ACHTUNG! FUNKTION NOCH NICHT IM EINSATZ!
+
+        //Checke ob sich was veraendert hat wen ja, kopiere gleich
+        for(int i=0; i < this.anzahlFelder; i++ ){
+            for(int j=0; j < this.anzahlFelder; j++ ){
+                if(this.spielFeldArray[i][j] != neuesFeld[i][j]){
+
                 }
             }
         }
     }
+
+    /**
+     * @see interfaces.Drawable
+     * @param g
+     */
+    public void drawObjects(Graphics g) {
+        // Jedes Feld soll sich selbst zeichnen
+        for(int i=0; i < this.anzahlFelder; i++){
+            for(int j=0; j < this.anzahlFelder; j++){
+                feld[i][j].drawObjects(g);
+            }
+        }
+    }
+
+    /**
+     * @see interfaces.Movable
+     * @param delta
+     */
+    public void doLogic(long delta) {
+
+        // Logik auf alle Felder anwenden
+        for(int i=0; i < this.anzahlFelder; i++){
+            for(int j=0; j < this.anzahlFelder; j++){
+                feld[i][j].doLogic(delta);
+            }
+        }
+
+    }
+
+    /**
+     *
+     * @see interfaces.Movable
+     * @param delta
+     */
+    public void move(long delta) {
+        // Bewegungen ausführen bei allen Feldern
+        for(int i=0; i < this.anzahlFelder; i++){
+            for(int j=0; j < this.anzahlFelder; j++){
+                feld[i][j].doLogic(delta);
+            }
+        }
+    }
+
 
 }
