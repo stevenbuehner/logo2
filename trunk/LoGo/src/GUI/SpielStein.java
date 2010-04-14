34203dce3a95fd346840d930541236caaea95293
Steven4Jesus <Steven4Jesus@0c3aa028-4a46-be0a-df05-f9b5da6e5ad4>
2010-04-14 13:50:14 +0000

    Erweitern von SpielStein auf halbdurchsichtige Grafiken (zum Beispiel runde Spielsteine)

--- trunk/LoGo/src/GUI/SpielStein.java
+++ trunk/LoGo/src/GUI/SpielStein.java
@@ -6,6 +6,7 @@ import interfaces.Movable;
 import java.awt.Color;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
 import java.awt.Polygon;
 import java.awt.Shape;
@@ -153,6 +154,18 @@ public abstract class SpielStein extends Rectangle2D.Double implements Movable, 
 	public void drawObjects(Graphics g) {
 		if ( !visible )
 			return;
+
+		Graphics2D g2 = (Graphics2D) g;
+		if ( angle != 0 ) {
+			at.rotate( Math.toRadians( angle ), x + rotationx, y + rotationy );
+			g2.setTransform( at );
+			g2.drawImage( pics[currentpic], (int) x, (int) y, null );
+			at.rotate( -Math.toRadians( angle ), x + rotationx, y + rotationy );
+			g2.setTransform( at );
+		}
+		else {
+			g.drawImage( pics[currentpic], (int) x, (int) y, null );
+		}
 	}
 
 	/**
@@ -313,6 +326,73 @@ public abstract class SpielStein extends Rectangle2D.Double implements Movable, 
 
 	public Point getRotation() {
 		return (new Point( rotationx, rotationy ));
+	}
+
+
+        public boolean checkOpaqueColorCollisions(SpielStein s) {
+
+		Rectangle2D.Double cut = (Double) this.createIntersection( s );
+		if ( (cut.width < 1) || (cut.height < 1) ) {
+			return false;
+		}
+
+		// Rechtecke in Bezug auf die jeweiligen Images
+		Rectangle2D.Double sub_me = getSubRec( this, cut );
+		Rectangle2D.Double sub_him = getSubRec( s, cut );
+
+		BufferedImage img_me = pics[currentpic].getSubimage( (int) sub_me.x, (int) sub_me.y,
+				(int) sub_me.width, (int) sub_me.height );
+		BufferedImage img_him = s.pics[s.currentpic].getSubimage( (int) sub_him.x, (int) sub_him.y,
+				(int) sub_him.width, (int) sub_him.height );
+
+		for (int i = 0; i < img_me.getWidth(); i++) {
+			for (int n = 0; n < img_him.getHeight(); n++) {
+
+				int rgb1 = img_me.getRGB( i, n );
+				int rgb2 = img_him.getRGB( i, n );
+
+				if ( isOpaque( rgb1 ) && isOpaque( rgb2 ) ) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	protected Rectangle2D.Double getSubRec(Rectangle2D.Double source, Rectangle2D.Double part) {
+
+		// Rechtecke erzeugen
+		Rectangle2D.Double sub = new Rectangle2D.Double();
+
+		// get X - compared to the Rectangle
+		if ( source.x > part.x ) {
+			sub.x = 0;
+		}
+		else {
+			sub.x = part.x - source.x;
+		}
+
+		if ( source.y > part.y ) {
+			sub.y = 0;
+		}
+		else {
+			sub.y = part.y - source.y;
+		}
+
+		sub.width = part.width;
+		sub.height = part.height;
+
+		return sub;
+	}
+
+	protected boolean isOpaque(int rgb) {
+
+		int alpha = (rgb >> 24) & 0xff;
+
+		if ( alpha == 0 ) {
+			return false;
+		}
+		return true;
 	}
 
 }
