f0c3011910bef85db5e4eac6cb8519d221af3178
t.schladitz <t.schladitz@0c3aa028-4a46-be0a-df05-f9b5da6e5ad4>
2010-04-23 09:33:21 +0000

    Einstellungsfenster angefangen

---
+++ LoGo/src/GUI/SpieleinstellungFenster.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package GUI;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author tommy
+ */
+public class SpieleinstellungFenster extends JFrame implements MouseListener, ActionListener{
+    public SpieleinstellungFenster(){
+
+    }
+
+    private void init(){
+
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mousePressed(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mouseReleased(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mouseEntered(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mouseExited(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
