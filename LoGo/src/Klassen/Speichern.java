5d27169af6a655da94c1e1d7a45a8d63452f0564
beccy.king <beccy.king@0c3aa028-4a46-be0a-df05-f9b5da6e5ad4>
2010-04-27 08:47:20 +0000

    Erstellen der Klasse Speichern

---
+++ LoGo/src/Klassen/Speichern.java
@@ -0,0 +1,97 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Klassen;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author beccy
+ */
+public class Speichern{
+
+    private String spielzuege;
+    private Spielfeld spielfeld;
+    private int spielfeldArray[][];
+    private Spieler spieler_weiss;
+    private Spieler spieler_schwarz;
+    private int spielfeldgroesse[][];
+    private float komipunkte;
+    private int vorgabesteine;
+
+    public void SpeicherSpiel(){
+
+            spieler_schwarz = spielfeld.getSpielerSchwarz();
+            spieler_weiss = spielfeld.getSpielerWeiss();
+            spielfeldgroesse = spielfeld.getAktuelesSpielFeld();
+            komipunkte = spieler_weiss.getKomiPunkte();
+            vorgabesteine = spielfeld.getVorgabeZahl();
+
+            //Startbelegung
+            spielfeldArray = spielfeld.getSpielfeldZumZeitpunkt(0);
+
+            //Standardkonfigurationen
+            spielzuege+="(;GM[1]FF[4]RU[Japanese]";
+
+            //Spielfeldgroesse
+            spielzuege+="SZ["+spielfeldgroesse+"]";
+            
+            //Vorgabesteine
+            spielzuege+="HA["+vorgabesteine+"]";
+
+            //Komipunkte
+            spielzuege+="KM["+komipunkte+"]";
+
+            //Spielernamen
+            spielzuege+="PW["+spieler_weiss.getSpielerName()+"]";
+            spielzuege+="PB["+spieler_schwarz.getSpielerName()+"]";
+
+            //Standardkonfigurationen
+            spielzuege+="GN[LoGo]DT[Default]TM[Default]";
+
+            int farbe = 0;
+            int x = 0;
+            int y = 0;
+
+            for(int i=0;i<(spielfeld.getspielZugCollection().size());i++)
+            {
+
+                farbe = spielfeld.getspielZugCollection().get(i).getFarbe();
+                x = spielfeld.getspielZugCollection().get(i).getXPosition();
+                y = spielfeld.getspielZugCollection().get(i).getYPosition();
+
+                if(farbe==1)
+                {//Schwarzer Stein
+                        spielzuege += "S["+x+y+"]";
+                }
+                else if(farbe ==2)
+                {//Weisser Stein
+                    spielzuege += "W["+x+y+"]";
+                }
+
+                else
+                {
+                    break;
+                }
+            }
+
+            PrintWriter pw = null;
+            try {
+            BufferedWriter bw = new BufferedWriter( new FileWriter( "C:/Go-Spiel.txt"));
+            pw = new PrintWriter( bw );
+
+            pw.println(spielzuege);
+        } catch ( IOException e ){
+            System.out.println( "Konnte Datei nicht erstellen" );
+        } finally {
+            if ( pw != null )
+                pw.close();
+        }
+     }
+}
