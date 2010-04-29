98487973b29865d50f22998875902debac619753
Steven4Jesus <Steven4Jesus@0c3aa028-4a46-be0a-df05-f9b5da6e5ad4>
2010-04-29 11:57:24 +0000

    Initialisierung des Fensters fuer die Auswertung

---
+++ LoGo/src/GUI/FensterAuswertung.java
@@ -0,0 +1,104 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package GUI;
+
+import Klassen.Spielfeld;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRootPane;
+
+/**
+ *
+ * @author steven
+ * @version 0.2
+ */
+public class FensterAuswertung extends JFrame implements MouseListener{
+
+    BufferedImage backgroundImage;
+    JPanel contentPanel;
+    Spielfeld dasSpielfeld;
+    ImageIcon LogoIcon;
+    
+    public FensterAuswertung( ){
+        this.init();
+    }
+
+    private void init(){
+
+        GrafikLib lib =  GrafikLib.getInstance();
+        this.backgroundImage = lib.getSprite("GUI/resources/Auswertungsanzeige.jpg");
+        this.contentPanel = new BackgroundImagePanel(backgroundImage);
+        this.dasSpielfeld = null;
+       // this.LogoIcon = lib.getIcon(null)
+
+        this.addMouseListener(this);
+
+        this.setUndecorated(true);
+        this.getRootPane().setWindowDecorationStyle(JRootPane.NONE);
+        this.setContentPane(contentPanel);
+        this.setSize(1024,768);
+        this.setLocationRelativeTo(null);
+        //this.setVisible(true);
+    }
+
+    public void setAnzeige( Spielfeld dasFeld ){
+        this.dasSpielfeld = dasFeld;
+    }
+
+
+    @Override
+    public void paint(Graphics g){
+        super.paint(g);
+        g.drawImage(backgroundImage, 0, 0, null);
+
+        this.render(g);
+    
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        Object[] options = { "Abbrechen",
+                                "Neues Spiel starten",
+                                "Programm beenden" };
+
+        int antwort = JOptionPane.showOptionDialog(this, 
+                "Wie möchten Sie nun fortfahren?", 
+                "Zwischenfrage", 
+                JOptionPane.YES_NO_CANCEL_OPTION, 
+                JOptionPane.QUESTION_MESSAGE, 
+                null, 
+                options, 
+                options[0]);
+    }
+
+    public void mousePressed(MouseEvent e) {
+    }
+
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    public void mouseExited(MouseEvent e) {
+    }
+
+    private void render(Graphics g) {
+        if(this.dasSpielfeld != null){
+            // Zeichne die Informationen aus dem Spielfeld auf die Oberflaeche
+
+
+            //ImageIcon icon = new ImageIcon<
+        }
+    }
+
+
+}
