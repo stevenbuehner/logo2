55418ea3877b4941fa7b779bda1dca53cb7a43c8
t.schladitz <t.schladitz@0c3aa028-4a46-be0a-df05-f9b5da6e5ad4>
2010-05-02 07:16:19 +0000

    OK, bei mir lief der mit den Packagenamen nicht, sind jetzt alle gross.

--- LoGo/src/Klassen/Steuerung.java
+++ LoGo/src/Klassen/Steuerung.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
-import logo.LoGoApp;
+import Logo.LoGoApp;
 
 /**
  * Die Klasse dient zur Steuerung des Spielflusses
