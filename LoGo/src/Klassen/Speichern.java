4afad567234d3261c05a55c18e180ce617d0c9ba
beccy.king@gmail.com <beccy.king@gmail.com@0c3aa028-4a46-be0a-df05-f9b5da6e5ad4>
2010-05-04 06:54:34 +0000

    Fertigstellung der Klasse Speichern: Hinzufuegen von TM, DT, Ueberpruefung von Speicherfenster, Speicherformat, Gesamtablauf

--- LoGo/src/Klassen/Speichern.java
+++ LoGo/src/Klassen/Speichern.java
@@ -9,24 +9,36 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.swing.JFileChooser;
 import logo.LoGoApp;
 
 /**
- *
+ * Diese Klasse uebernimmt den Speichervorgang
+ * Es wird im SGF-Format abgespeichert: Startbelegung, Spielernamen, Spielzuege,
+ * Standardkonfigurationen von SGF, Vorgabesteine, Komipunkte, Spielfeldgroesse
  * @author beccy
  * @version 0.2
  */
 public class Speichern {
 
-    private String spielzuege;
+    //Lokale Variablen
+    private String spielzuege; 
     private Spielfeld spielfeld;
     private int spielfeldArray[][];
     private Spieler spieler_weiss;
     private Spieler spieler_schwarz;
+    private long spieler_weiss_spielzeit;
+    private long spieler_schwarz_spielzeit;
     private int spielfeldgroesse;
     private float komipunkte;
     private int vorgabesteine;
+    private long periodenzeit;
+    private Date aktuellesDatum;
+    private SimpleDateFormat aktuellesDatum_format;
+    private Date aktuelleZeit;
+     private SimpleDateFormat aktuelleZeit_format;
 
     /**
      * Die Klasse bekommt mit dem Konstruktor das @param zuSpeicherndesSpielfeld
@@ -35,18 +47,35 @@ public class Speichern {
      * übertragen würden.
      */
     Speichern(Spielfeld zuSpeicherndesSpielfeld) {
+        //Instanz von Spielfeld wird der Speicherklasse im Konstruktor übergeben
         this.spielfeld = zuSpeicherndesSpielfeld;
     }
 
-    public void SpeicherSpiel() {
+    public void SpeicherSpiel( ){
+
+        //Get-Funktionen für die lokalen Variablen
         spieler_schwarz = spielfeld.getSpielerSchwarz();
         spieler_weiss = spielfeld.getSpielerWeiss();
         spielfeldgroesse = spielfeld.getSpielfeldGroesse();
         komipunkte = spieler_weiss.getKomiPunkte();
         vorgabesteine = spielfeld.getVorgabeZahl();
+        periodenzeit = spielfeld.getPeriodenZeit();
+        spieler_schwarz_spielzeit = spielfeld.getSpielerSchwarz().getVerbleibendeSpielzeitInMS();
+        spieler_weiss_spielzeit = spielfeld.getSpielerWeiss().getVerbleibendeSpielzeitInMS();
+        
+        //Datumsformat festlegen
+        aktuellesDatum = new Date();
+        aktuellesDatum_format = new SimpleDateFormat("dd-MM-yyyy");
 
+        //Zeitformat festlegen
+        aktuelleZeit = new Date();
+        aktuelleZeit_format = new SimpleDateFormat("HH:mm:ss");
+              
         //Startbelegung
         spielfeldArray = spielfeld.getSpielfeldZumZeitpunkt(0);
+
+        //spielzuege leeren
+        spielzuege = "";
 
         //Standardkonfigurationen
         spielzuege += "(;GM[1]FF[4]RU[Japanese]";
@@ -64,42 +93,81 @@ public class Speichern {
         spielzuege += "PW[" + spieler_weiss.getSpielerName() + "]";
         spielzuege += "PB[" + spieler_schwarz.getSpielerName() + "]";
 
+        //Spielzeiten der Spieler
+        spielzuege += "TW["+spieler_weiss_spielzeit / 1000+"]";
+        spielzuege += "TB["+spieler_schwarz_spielzeit / 1000+"]";
+
         //Standardkonfigurationen
-        spielzuege += "GN[LoGo]DT[Default]TM[Default]";
+        spielzuege += "GN[LoGo]";
 
+        //Datum
+        spielzuege += "DT["+aktuellesDatum_format.format(aktuellesDatum)+"]";
+
+        //Zeit
+        spielzuege += "TM["+aktuelleZeit_format.format(aktuelleZeit)+"]";
+
+        //Periodenzeit
+        spielzuege += "OT["+periodenzeit / 1000+"]";
+
+        //Farb-Identifier: 1 für Schwarz | 2 für Weiss
         int farbe = 0;
-        int x = 0;
-        int y = 0;
 
-        for (int i = 0; i < (spielfeld.getspielZugCollection().size()); i++) {
+        //X-Koordinate
+        int x = 0; 
+        
+        //Y-Koordinate
+        int y = 0; 
 
+        //Für jeden Spielzug in der Collection
+        for(int i=0;i<(spielfeld.getspielZugCollection().size());i++)
+        {
             farbe = spielfeld.getspielZugCollection().get(i).getFarbe();
             x = spielfeld.getspielZugCollection().get(i).getXPosition();
             y = spielfeld.getspielZugCollection().get(i).getYPosition();
 
-            if (farbe == 1) {//Schwarzer Stein
-                spielzuege += "S[" + x + y + "]";
-            } else if (farbe == 2) {//Weisser Stein
-                spielzuege += "W[" + x + y + "]";
-            } else {
+            //Umrechnung der Koordinaten in Buchstaben: Anfang mit 'a' = 97
+            x += 97;
+            y += 97;
+
+            char _buchstabeX = (char)x;
+            char _buchstabeY = (char)y;
+
+            //Schwarzer Stein
+            if (farbe == 1)
+            {
+                spielzuege += "S["+_buchstabeX+_buchstabeY+"]";
+            }
+            //Weisser Stein
+            else if (farbe == 2)
+            {
+                spielzuege += "W["+_buchstabeX+_buchstabeY+"]";
+            } else
+            {
                 break;
             }
         }
 
-
-        /*
-         * Hi Beccy, bin da grad druebergestolpert und dachte das war doch so etwas,
-         * wie Du gesucht hast ... ?!? ... Wenn nicht einfach wieder loeschen ...
-         * LG, Steven
-         */
+        //Fenster zum Speichervorgang
         JFileChooser chooser = new JFileChooser();
-        chooser.showSaveDialog( (FensterSpieloberflaeche)LoGoApp.meineOberflaeche);
-        String selFile = chooser.getCurrentDirectory() + chooser.getSelectedFile().getName() + ".cfg";
+        chooser.showSaveDialog((FensterSpieloberflaeche)LoGoApp.meineOberflaeche);
 
+        //Directory und Dateiname definieren
+        String selFile = chooser.getCurrentDirectory() + "\\" + chooser.getSelectedFile().getName();
 
+        //Endung überprüfen
+        if(selFile.toLowerCase().contains(".cfg"))
+        {
+            // File Name enthält die Endung ".cfg"
+        }
+        else
+        {
+            selFile += ".cfg";
+        }
+
+        //Zusammengesetzten String in die Datei abspeichern
         PrintWriter pw = null;
         try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter(selFile));
+            BufferedWriter bw = new BufferedWriter(new FileWriter(selFile.toString()));
             pw = new PrintWriter(bw);
 
             pw.println(spielzuege);
@@ -110,5 +178,5 @@ public class Speichern {
                 pw.close();
             }
         }
-    }
+      }
 }
