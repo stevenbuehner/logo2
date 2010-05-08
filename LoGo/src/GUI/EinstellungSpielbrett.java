2651873d4b64f1fd3bdd0ade152c0111f349e135
Steven4Jesus <Steven4Jesus@0c3aa028-4a46-be0a-df05-f9b5da6e5ad4>
2010-05-08 22:18:58 +0000

    Formulierung der JavaDoc fuer die Klassen BackgroundImagePanel, EinstellungSpielbrett, FensterCredits und GrafikLib

--- LoGo/src/GUI/EinstellungSpielbrett.java
+++ LoGo/src/GUI/EinstellungSpielbrett.java
@@ -3,16 +3,43 @@ package GUI;
 import Klassen.Konstante;
 
 /**
- *
- * @author tommy
+ * Abgeleitete Klasse von Spielbrett, das speziell zur Darstellung des
+ * Spielbrettes innerhalb des Einstellungsfensters gedacht ist. Sie nimmt
+ * benötigte Änderungen innerhalb dieser vor und überschreibt die Methode
+ * updateSpielFeld(int[][] neuesSpielFeld).
+ * Dies ist nötig, da (anders wie im laufenden Spiel) Kugeln sowohl gesetzt,
+ * als auch entfernt werden können müssen.
+ * 
+ * @author tommy, Steven
  * @version 0.2
  */
 public class EinstellungSpielbrett extends Spielbrett {
 
+    /**
+     * Konstruktor der die Parameter alle direkt weiter an den Konstrutor der
+     * Vaterklasse Spielbrett leitet.
+     * @param breite Breite des zu zeichnenden Spielfeldes in Pixeln
+     * @param hoehe Höhe des zu zeichnenden Spielfeldes in Pixeln
+     * @param xOffset Abstand vom linken Rand des Fensters in Pixeln
+     * @param yOffset Abstand vom oberen Rand des Fensters in Pixeln
+     * @param anzahlFelder Anzahl der darzustellenden Felder in X- und Y-Richtung
+     * @see Spielbrett
+     */
     public EinstellungSpielbrett(int breite, int hoehe, int xOffset, int yOffset, int anzahlFelder) {
         super(breite, hoehe, xOffset, yOffset, anzahlFelder);
     }
 
+    /**
+     * Bei Änderungen des Spielfeldes wird hier ein
+     * Integer-Array mit den neuen Spieldaten übergeben. Die erlaubten Werte
+     * sind in der Klasse Konstante definiert. Möglich sind alle Konstanden,
+     * die mit SCHNITTPUNKT_ beginnen. 
+     * Die Klasse sorgt dafür, dass ÄNDERUNGEN animiert, bzw. in der GUI umgesetzt werden.
+     *
+     * @param neuesSpielFeld Integer-Array mit dem neuen Spielfeld
+     * @see Konstante
+     *
+     */
     @Override
     public void updateSpielFeld(int[][] neuesSpielFeld) {
 
