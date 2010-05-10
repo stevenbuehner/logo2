0a84830f617ec550107bb83b39b9725d4e13da28
beccy.king@gmail.com <beccy.king@gmail.com@0c3aa028-4a46-be0a-df05-f9b5da6e5ad4>
2010-05-10 07:06:08 +0000

    Abschluss, Anpassung an das SGF-Format

--- trunk/LoGo/src/Klassen/Laden.java
+++ trunk/LoGo/src/Klassen/Laden.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 import logo.LoGoApp;
 
 /**
@@ -14,10 +15,11 @@ import logo.LoGoApp;
 public class Laden {
 
     //lokale Variablen
-    private String _geladenerString;
+    private String _geladenerString = "";
     private Spieler spieler_weiss;
     private Spieler spieler_schwarz;
-    private char c_lokale_umwandlungsvariable; //Für Ascii - String Umwandlung
+    //Für Ascii - String Umwandlung
+    private char c_lokale_umwandlungsvariable; 
     public Spielfeld _spielfeld;
 
     //Funktion für den Identifier des Switch-Case Konstrukts
@@ -66,12 +68,12 @@ public class Laden {
             _id = 8;
         }
         //Spielzuege Schwarz
-        else if(id.equals("S"))
+        else if(id.equals(";S"))
         {
           _id = 9;
         }
         //Spielzuege Weiss
-        else if(id.equals("W"))
+        else if(id.equals(";W"))
         {
           _id = 10;
         }
@@ -94,181 +96,192 @@ public class Laden {
     public void LadeSpiel( ){
 
        JFileChooser chooser = new JFileChooser();
-       chooser.showOpenDialog((FensterSpieloberflaeche)LoGoApp.meineOberflaeche);
-       //String selFile = chooser.getCurrentDirectory()+ "\\" + chooser.getSelectedFile().getName();
-       String selFile = chooser.getSelectedFile().getAbsolutePath();
+       int rueckgabe =  chooser.showOpenDialog((FensterSpieloberflaeche)LoGoApp.meineOberflaeche);
 
-       try{
+       if(rueckgabe == JFileChooser.CANCEL_OPTION)
+       {
+           JOptionPane.showMessageDialog(null, "Ladevorgang wurde abgebrochen!");
+       }
+       else
+       {
+            String selFile = chooser.getSelectedFile().getAbsolutePath();
+            try{
             BufferedReader in = new BufferedReader(new FileReader (selFile));
             String zeile = null;
             while((zeile = in.readLine()) != null)
             {
-                _geladenerString = zeile;
+                _geladenerString += zeile;
             }
-        }catch(IOException e){
-        }
 
-    //Positionsvariablen von den Klammern
-    int _klammer_auf;
-    int _klammer_zu;
-    
-    String identifier = null;
+            }catch(IOException e){
+            }
 
-    //Spielerobjekte erzeugen
-    long l_spieler_variable = 0;
-    float f_spieler_variable = 0;
-    String s_spieler_variable = "";
-    int i_spieler_variable = 0;
-    spieler_weiss = new Spieler(s_spieler_variable,l_spieler_variable,i_spieler_variable,f_spieler_variable);
-    spieler_schwarz = new Spieler(s_spieler_variable,l_spieler_variable,i_spieler_variable,f_spieler_variable);
+            //Positionsvariablen von den Klammern
+            int _klammer_auf;
+            int _klammer_zu;
 
-    //Herausschneidender Standard_SGF_Konfiguration
-    _klammer_auf = _geladenerString.indexOf("(;") + 2;
-    _geladenerString = _geladenerString.substring(_klammer_auf);
+            String identifier = null;
 
-    //Ein Identifier besteht maximal aus zwei Ziffern
-    char c1;
-    char c2;
-    
-    String s_parameter  = null;
+            //Spielerobjekte erzeugen
+            long l_spieler_variable = 0;
+            float f_spieler_variable = 0;
+            String s_spieler_variable = "";
+            int i_spieler_variable = 0;
+            spieler_weiss = new Spieler(s_spieler_variable,l_spieler_variable,i_spieler_variable,f_spieler_variable);
+            spieler_schwarz = new Spieler(s_spieler_variable,l_spieler_variable,i_spieler_variable,f_spieler_variable);
 
-    //Deklaration von Parametervariablen als Integerzahl, Float und Long
-    int i_parameter = 0;
-    float f_parameter = 0;
-    long l_parameter = 0;
+            //Herausschneidender Standard_SGF_Konfiguration
+            _klammer_auf = _geladenerString.indexOf("(;") + 2;
+            _geladenerString = _geladenerString.substring(_klammer_auf);
 
-    //Parametervariablen für die Spielzüge als String
-    String s_parameterX;
-    String s_parameterY;
+            //Ein Identifier besteht maximal aus zwei Ziffern
+            char c1;
+            char c2;
 
-    //Parametervariablen für die Spielzüge als Integerzahl
-    int i_parameterX = 0;
-    int i_parameterY = 0;
+            String s_parameter  = null;
 
-    //Durchfuehren der Schleife bis der String keinen Inhalt mehr besitzt
-    do
-    {
-        _klammer_auf = _geladenerString.indexOf("[");
+            //Deklaration von Parametervariablen als Integerzahl, Float und Long
+            int i_parameter = 0;
+            float f_parameter = 0;
+            long l_parameter = 0;
 
+            //Parametervariablen für die Spielzüge als String
+            String s_parameterX;
+            String s_parameterY;
 
-       c1 = _geladenerString.charAt(0);
-       identifier = String.valueOf(c1);
+            //Parametervariablen für die Spielzüge als Integerzahl
+            int i_parameterX = 0;
+            int i_parameterY = 0;
 
-       //Wenn der Identifier aus mehr als einem Buchstaben besteht
-       if(_klammer_auf > 1)
-       {
-        c2 = _geladenerString.charAt(1);
-        identifier += String.valueOf(c2);
+            //Durchfuehren der Schleife bis der String keinen Inhalt mehr besitzt
+            do
+            {
+                _klammer_auf = _geladenerString.indexOf("[");
+                c1 = _geladenerString.charAt(0);
+                identifier = String.valueOf(c1);
+
+                //Wenn der Identifier aus mehr als einem Buchstaben besteht
+                if(_klammer_auf > 1)
+                {
+                    c2 = _geladenerString.charAt(1);
+                    identifier += String.valueOf(c2);
+                }
+
+                _klammer_zu = _geladenerString.indexOf("]");
+
+                //Parameterzuweisung
+                s_parameter = _geladenerString.substring(_klammer_auf + 1, _klammer_zu);
+
+                /*
+                 * Je nach Identifier wird der Parameter als Integerzahl, Float, Long oder
+                 * String interpretiert und verwendet
+                 */
+                switch(this.getID(identifier)){
+                    case 1:
+                        /* Spielfeldgröße
+                         * Umwandeln von String zu Intergerzahl für die Spielfeldgroesse
+                         */
+                        i_parameter = Integer.parseInt(s_parameter);
+
+                        //Erzeuge Spielfeld und weise der lokalen Variablen das neue Spielfeld zu
+                        this._spielfeld = new Spielfeld(i_parameter);
+
+                        //Weise dem Spielfeld die beiden Spieler zu
+                        _spielfeld.setSpielerSchwarz(spieler_schwarz);
+                        _spielfeld.setSpielerWeiss(spieler_weiss);
+                        break;
+                    case 2:
+                        //Umwandeln von String zu Integerzahl für Vorgabesteine
+                        i_parameter = Integer.parseInt(s_parameter);
+                        this._spielfeld.initialisiereFeldMitVorgabenFuerSchwarz(i_parameter);
+                        break;
+                    case 3:
+                        //Umwandeln von String zu Float für Komipunkte
+                        f_parameter = Float.parseFloat(s_parameter);
+                        this.spieler_weiss.setKomiPunkte(f_parameter);
+                        break;
+                    case 4:
+                        //Name Spieler - Weiss
+                        spieler_weiss.setSpielerName(s_parameter);
+                        break;
+                    case 5:
+                        //Name Spieler - Schwarz
+                        spieler_schwarz.setSpielerName(s_parameter);
+                        break;
+                    case 6:
+                        //Umwandeln von String zu Long für Spielzeit
+                        l_parameter = Long.parseLong(s_parameter);
+                        this.spieler_weiss.setVerbleibendeSpielzeitInMS(l_parameter * 1000);
+                        break;
+                    case 7:
+                        //Umwandeln von String zu Long für verbleibende Zeit
+                        l_parameter = Long.parseLong(s_parameter);
+                        this.spieler_schwarz.setVerbleibendeSpielzeitInMS(l_parameter * 1000);
+                        break;
+                    case 8:
+                        //Umwandeln von String zu Long für Periodenzeit
+                        s_parameter = (String) s_parameter.subSequence(0, 2);
+                        l_parameter = Long.parseLong(s_parameter);
+                        this._spielfeld.setPeriodenZeit(l_parameter * 1000);
+                        break;
+                    case 9:
+                        /*
+                         * Koordinaten wenn als Identifier 'B' erscheint
+                         * Trenne X-Buchstabe von Y-Buchstabe
+                         */
+                        s_parameterX = s_parameter.substring(0,1);
+                        s_parameterY = s_parameter.substring(1,2);
+
+                        //Umwandlung und weise Koordinaten lokalen Variablen zu
+
+                        c_lokale_umwandlungsvariable = s_parameterX.charAt(0);
+                        i_parameterX = (int) c_lokale_umwandlungsvariable;
+
+                        c_lokale_umwandlungsvariable = s_parameterY.charAt(0);
+                        i_parameterY = (int) c_lokale_umwandlungsvariable;
+
+                        //Ziehe das 'a' = 97 wieder ab um die Koordinate als Zahl zu bekommen
+                        i_parameterX -= 97;
+                        i_parameterY -= 97;
+
+                        //Mache Zug mit Farbe = 1 für Spieler schwarz
+                        this._spielfeld.macheZug(i_parameterX, i_parameterY, 1);
+                        break;
+                    case 10:
+                        /*
+                         * Koordinaten wenn als Identifier 'W' erscheint
+                         * Siehe case 9
+                         */
+                        s_parameterX = s_parameter.substring(0,1);
+                        s_parameterY = s_parameter.substring(1,2);
+
+                        //Umwandlung von String zu Char
+                        c_lokale_umwandlungsvariable = s_parameterX.charAt(0);
+                        //Umwandlung von Char zu Ascii wert
+                        i_parameterX = (int) c_lokale_umwandlungsvariable;
+
+                        c_lokale_umwandlungsvariable = s_parameterY.charAt(0);
+                        i_parameterY = (int) c_lokale_umwandlungsvariable;
+
+                        //97 abziehen um die Koordinate zu bekommen
+                        i_parameterX -= 97;
+                        i_parameterY -= 97;
+
+                        //Mache Zug mit Farbe = 2 für Spieler weiss
+                        this._spielfeld.macheZug(i_parameterX, i_parameterY, 2);
+                        break;
+                    default:
+                        break;
+                }
+
+                //Schneidet aus dem Originalstring die schließende Klammer heraus
+                _geladenerString = _geladenerString.substring(_klammer_zu + 1);
+                //Letzte Zeichen Abfragen
+                if( _geladenerString.equals(")"))
+                {
+                    _geladenerString = (String) _geladenerString.subSequence(0, 0);
+                }
+            }while(!_geladenerString.isEmpty());
        }
-
-       _klammer_zu = _geladenerString.indexOf("]");
-
-       //Parameterzuweisung
-       s_parameter = _geladenerString.substring(_klammer_auf+1, _klammer_zu);
-       
-    /*
-     * Je nach Identifier wird der Parameter als Integerzahl, Float, Long oder
-     * String interpretiert und verwendet
-     */
-    switch(this.getID(identifier)){
-        case 1:
-            /* Spielfeldgröße
-             * Umwandeln von String zu Intergerzahl für die Spielfeldgroesse
-             */
-            i_parameter = Integer.parseInt(s_parameter);
-
-            //Erzeuge Spielfeld und weise der lokalen Variablen das neue Spielfeld zu
-            this._spielfeld = new Spielfeld(i_parameter);
-
-            //Weise dem Spielfeld die beiden Spieler zu
-            _spielfeld.setSpielerSchwarz(spieler_schwarz);
-            _spielfeld.setSpielerWeiss(spieler_weiss);
-            break;
-        case 2: 
-            //Umwandeln von String zu Integerzahl für Vorgabesteine
-            i_parameter = Integer.parseInt(s_parameter);
-            this._spielfeld.initialisiereFeldMitVorgabenFuerSchwarz(i_parameter);
-            break;
-        case 3:
-            //Umwandeln von String zu Float für Komipunkte
-            f_parameter = Float.parseFloat(s_parameter);
-            this.spieler_weiss.setKomiPunkte(f_parameter);
-            break;
-        case 4:
-            //Name Spieler - Weiss
-            spieler_weiss.setSpielerName(s_parameter);
-            break;
-        case 5:
-            //Name Spieler - Schwarz
-            spieler_schwarz.setSpielerName(s_parameter);
-            break;
-        case 6:
-            //Umwandeln von String zu Long für Spielzeit
-            l_parameter = Long.parseLong(s_parameter);
-            this.spieler_weiss.setVerbleibendeSpielzeitInMS(l_parameter * 1000);
-            break;
-        case 7:
-            //Umwandeln von String zu Long für verbleibende Zeit
-            l_parameter = Long.parseLong(s_parameter);
-            this.spieler_schwarz.setVerbleibendeSpielzeitInMS(l_parameter * 1000);
-            break;
-        case 8:
-            //Umwandeln von String zu Long für Periodenzeit
-            l_parameter = Long.parseLong(s_parameter);
-            this._spielfeld.setPeriodenZeit(l_parameter * 1000);
-            break;
-        case 9:
-            /*
-             * Koordinaten wenn als Identifier 'B' erscheint
-             * Trenne X-Buchstabe von Y-Buchstabe
-             */
-            s_parameterX = s_parameter.substring(0,1);
-            s_parameterY = s_parameter.substring(1,2);
-
-            //Umwandlung und weise Koordinaten lokalen Variablen zu
-
-            c_lokale_umwandlungsvariable = s_parameterX.charAt(0);
-            i_parameterX = (int) c_lokale_umwandlungsvariable;
-
-             c_lokale_umwandlungsvariable = s_parameterY.charAt(0);
-             i_parameterY = (int) c_lokale_umwandlungsvariable;
-            //Ziehe das 'a' = 97 wieder ab um die Koordinate als Zahl zu bekommen
-            i_parameterX -= 97;
-            i_parameterY -= 97;
-
-            //Mache Zug mit Farbe = 1 für Spieler schwarz
-            this._spielfeld.macheZug(i_parameterX, i_parameterY, 1);
-            break;
-        case 10:
-            /*
-             * Koordinaten wenn als Identifier 'W' erscheint
-             * Siehe case 9
-             *
-             */
-            s_parameterX = s_parameter.substring(0,1);
-            s_parameterY = s_parameter.substring(1,2);
-            
-            //Umwandlung von String zu Char
-            c_lokale_umwandlungsvariable = s_parameterX.charAt(0);
-            //Umwandlung von Char zu Ascii wert
-            i_parameterX = (int) c_lokale_umwandlungsvariable;
-
-            c_lokale_umwandlungsvariable = s_parameterY.charAt(0);
-            i_parameterY = (int) c_lokale_umwandlungsvariable;
-
-            //97 abziehen um die Koordinate zu bekommen
-            i_parameterX -= 97;
-            i_parameterY -= 97;
-
-            //Mache Zug mit Farbe = 2 für Spieler weiss
-            this._spielfeld.macheZug(i_parameterX, i_parameterY, 2);
-            break;
-
-        default:
-            break;
-        }
-        //Schneidet aus dem Originalstring die schließende Klammer heraus
-        _geladenerString = _geladenerString.substring(_klammer_zu + 1);
-        }while(!_geladenerString.isEmpty());
     }
 }
